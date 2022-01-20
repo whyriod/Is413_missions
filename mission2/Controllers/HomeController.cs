a8a706c7961ea69d5d18e9b5ad9a6f494873323f
Caleb Siebach <whyriod@byu.edu>
2022-01-19 17:57:42 -0700

    Adding to mission3

---
+++ mission2/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace missions.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Main Page
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Calculator View
+        public IActionResult Calc()
+        {
+            return View();
+        }
+    }
+}
